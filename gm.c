eb21e336f1f8c53dff079884f8d304e6ca825869
Stafeev <dmistf@gmail.com>
2020-02-17 23:23:44 +0700

    gm.c changed

---
+++ gm.c
@@ -0,0 +1,41 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define CSQ(a) (3.14*a*a)
+#define CPL(b) (6.28*b)
+int ccheck(float* x1,float* y1,float* x2,float* y2,float* r1,float* r2)
+{
+	float f;
+	f=sqrtf((*x1)-(*x2))*((*x1)-(*x2))+((*y1)-(*y2))*((*y1)-(*y2));
+	f=sqrtf(f);
+	if((r1-r2<f)&&(f<r1-r2)) printf("Circles (%f:%f) with radius %f and (%f:%f) with radius %f are crossing",*x1,*y1,*r1,*x2,*y2,*r2);
+}
+int main()
+{
+	float figure[4][2];
+	printf("First circle\n");
+	printf("Center point is ");
+	scanf("%f",&figure[0][0]);
+	scanf("%f",&figure[0][1]);			
+	printf("\n");
+	printf("Radius  ");
+	scanf("%f",&figure[1][1]);	
+	printf("\n");
+	printf("Square is %f",CSQ(figure[1][1]));
+	printf("\n");
+	printf("Perimeter is %f",CPL(figure[1][1]));			
+	printf("\n");
+	printf("Second circle\n");
+	printf("Center point is ");
+	scanf("%f",&figure[2][0]);
+	scanf("%f",&figure[2][1]);			
+	printf("\n");
+	printf("Radius  ");
+	scanf("%f",&figure[3][1]);	
+	printf("\n");	
+	printf("Square is %f",CSQ(figure[3][1]));
+	printf("\n");
+	printf("Perimeter is %f\n",CPL(figure[3][1]));	
+	ccheck(&figure[0][0],&figure[0][1],&figure[2][0],&figure[2][1],&figure[1][1],&figure[3][1]);
+	return 0;
+}
