0200b865f0175201cfa0833c6039907c2c82217d
Stafeev <dmistf@gmail.com>
2020-02-18 09:35:27 +0700

    gm.c works only with 2 circles

--- gm.c
+++ gm.c
@@ -6,9 +6,9 @@
 int ccheck(float* x1,float* y1,float* x2,float* y2,float* r1,float* r2)
 {
 	float f;
-	f=sqrtf((*x1)-(*x2))*((*x1)-(*x2))+((*y1)-(*y2))*((*y1)-(*y2));
-	f=sqrtf(f);
-	if((r1-r2<f)&&(f<r1-r2)) printf("Circles (%f:%f) with radius %f and (%f:%f) with radius %f are crossing",*x1,*y1,*r1,*x2,*y2,*r2);
+	f=sqrtf(((*x1)-(*x2))*((*x1)-(*x2))+((*y1)-(*y2))*((*y1)-(*y2)));
+	if(((*r1)-(*r2)<f)&&(f<(*r1)+(*r2))) printf("Circles (%f:%f) with radius %f and (%f:%f) with radius %f are crossing\n",*x1,*y1,*r1,*x2,*y2,*r2);
+	printf("Done\n");
 }
 int main()
 {
