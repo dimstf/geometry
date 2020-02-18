2ce9c62a90c27eab7e07071147ea76a34804b6d2
Stafeev <dmistf@gmail.com>
2020-02-18 10:55:22 +0700

    gm.c can work with 3 and more circles

--- gm.c
+++ gm.c
@@ -3,39 +3,40 @@
 #include <stdio.h>
 #define CSQ(a) (3.14*a*a)
 #define CPL(b) (6.28*b)
-int ccheck(float* x1,float* y1,float* x2,float* y2,float* r1,float* r2)
+float ccheck(float* x1,float* y1,float* x2,float* y2,float* r1,float* r2)
 {
 	float f;
 	f=sqrtf(((*x1)-(*x2))*((*x1)-(*x2))+((*y1)-(*y2))*((*y1)-(*y2)));
 	if(((*r1)-(*r2)<f)&&(f<(*r1)+(*r2))) printf("Circles (%f:%f) with radius %f and (%f:%f) with radius %f are crossing\n",*x1,*y1,*r1,*x2,*y2,*r2);
-	printf("Done\n");
+	else printf("Circles (%f:%f) with radius %f and (%f:%f) with radius %f are NOT crossing\n",*x1,*y1,*r1,*x2,*y2,*r2);
 }
 int main()
 {
-	float figure[4][2];
-	printf("First circle\n");
+	int n,t,k,r;
+	printf("Input number of figures: ");	
+	scanf("%d",&n);
+	r=0;
+	t=2*n;
+	float figure[t][3];
+	for(k=n;k>0;k--){
+	printf("Circle\n");
 	printf("Center point is ");
-	scanf("%f",&figure[0][0]);
-	scanf("%f",&figure[0][1]);			
+	scanf("%f",&figure[r][0]);
+	scanf("%f",&figure[r][1]);			
 	printf("\n");
 	printf("Radius  ");
-	scanf("%f",&figure[1][1]);	
+	scanf("%f",&figure[r][2]);	
 	printf("\n");
-	printf("Square is %f",CSQ(figure[1][1]));
+	printf("Square is %f",CSQ(figure[r][2]));
 	printf("\n");
-	printf("Perimeter is %f",CPL(figure[1][1]));			
-	printf("\n");
-	printf("Second circle\n");
-	printf("Center point is ");
-	scanf("%f",&figure[2][0]);
-	scanf("%f",&figure[2][1]);			
-	printf("\n");
-	printf("Radius  ");
-	scanf("%f",&figure[3][1]);	
+	printf("Perimeter is %f",CPL(figure[r][2]));			
 	printf("\n");	
-	printf("Square is %f",CSQ(figure[3][1]));
-	printf("\n");
-	printf("Perimeter is %f\n",CPL(figure[3][1]));	
-	ccheck(&figure[0][0],&figure[0][1],&figure[2][0],&figure[2][1],&figure[1][1],&figure[3][1]);
+	r++;
+	}
+	r=0;
+	for(k=0;k<n;k++){
+		for(t=k;t<n-1;t++)
+		ccheck(&figure[t][0],&figure[t][1],&figure[t+1][0],&figure[t+1][1],&figure[t][2],&figure[t+1][2]);
+	}
 	return 0;
 }
