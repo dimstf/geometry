1564fd086373574bb94d4f8a87783ac5cfefff34
StaffevDA <dimstf@gmail.com>
2020-05-24 17:14:32 +0700

    include math.h in func.c

--- func.c
+++ func.c
@@ -1,5 +1,6 @@
-#define CSQ(a) (3.14*a*a)
-#define CPL(b) (6.28*b)
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 float ccheck(float* x1,float* y1,float* x2,float* y2,float* r1,float* r2)
 {
 	float f;
