8b7b530e45e9509eeb93c102a7a9ff8354bbdff1
dimstf <58879992+dimstf@users.noreply.github.com>
2020-05-27 22:02:09 +0700

    another place for -lm

--- tr.sh
+++ tr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -lm -Wall -Werror -c -o build/func.o func.c
+gcc -Wall -Werror -lm -c -o build/func.o func.c
 gcc -Wall -Werror -c -o build/gm.o gm.c
 gcc build/func.o build/gm.o -o bin/circles
 rm build/func.o build/gm.o
