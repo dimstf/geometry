2f8b8aaa180ed1d10018f1fda10e4fbf0124fe7a
StaffevDA <dimstf@gmail.com>
2020-05-27 23:00:41 +0700

    Modify func.c for test and create simple test for checking equation

--- test/test1.c
+++ test/test1.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "ctest.h"
+#include "func.c"
 int main()
 {
+	float a1=0,a2=0,rad1=1,b1=1,b2=1,rad2=2
+	float res,real=;
+	res=check(&a1,&a2,&b1,&b2,&rad1,&rad2);
+	ASSERT_EQUAL_FLOAT(res,1.0);
 	return 0;
 }
