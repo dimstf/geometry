796750cd209e53fbe5c1e87baebe0b30f5e1f4dc
dimstf <58879992+dimstf@users.noreply.github.com>
2020-05-27 21:45:31 +0700

    New version of tr.sh

--- tr.sh
+++ tr.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-make
+gcc -lm -Wall -Werror -c -o build/func.o func.c
+gcc -Wall -Werror -c -o build/gm.o gm.c
+gcc -lm  build/func.o build/gm.o -o bin/circles
+rm build/func.o build/gm.o bin/circles
