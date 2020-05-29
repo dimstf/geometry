161484a5c12597f01b0bdeeb3fdffac080f04927
dimstf <58879992+dimstf@users.noreply.github.com>
2020-05-29 12:28:36 +0700

    Update tr.sh

--- tr.sh
+++ tr.sh
@@ -4,3 +4,9 @@ gcc -lm -Wall -Werror -c -o build/gm.o gm.c
 gcc -lm build/func.o build/gm.o -o bin/circles
 rm build/func.o build/gm.o
 echo "Done"
+gcc -lm -Wall -Werror -c -o build/functest.o func.c
+gcc -lm -Wall -Werror -c -o build/test.o test/test1.c
+gcc -lm build/test.o build/functest.o -o bin/test
+cd bin
+echo "test"
+./test
