71995ad487e6c1c9181a6c737fda8e29dcdb2680
dimstf <58879992+dimstf@users.noreply.github.com>
2020-05-29 12:29:00 +0700

    Delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-gcc -lm -Wall -Werror -c -o build/functest.o func.c
-gcc -lm -Wall -Werror -c -o build/test.o test/test1.c
-gcc -lm build/test.o build/functest.o -o bin/test
-cd bin
-echo "test"
-./test
